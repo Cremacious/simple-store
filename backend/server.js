c603066721c0f02f1192bbbe1297abcb402bf6bf
cremacious <chrismackall3@gmail.com>
2025-09-26 10:50:44 -0400

    Added controllers for put, get, delete, and post.

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,12 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
+import productRoutes from './routes/product.route.js';
 
 const app = express();
 app.use(express.json());
 const PORT = 3001;
 
-app.post('/api/products', async (req, res) => {
-  const product = req.body;
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'All fields are required' });
-  }
-  const newProduct = new Product(product);
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    res.status(500).json({ success: false, message: 'Server Error' });
-  }
-});
-
-app.delete('/api/products/:id', async (req, res) => {
-    try
-})
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
   connectDB();
